fd0a2458580512d2a3e38cd213f1f39fce5baf12
Ramón Román Castro <ramonromancastro@gmail.com>
2021-06-08 11:37:16 +0200

    Update download.php

--- share/download.php
+++ share/download.php
@@ -15,7 +15,7 @@ if (file_exists($realpath)){
 	header('Expires: 0');
 	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 	header('Cache-Control: private', false); // required for certain browsers 
-	header('Content-Type: application/pdf');
+	// header('Content-Type: application/pdf');
 
 	header('Content-Disposition: attachment; filename="'. basename($realpath) . '";');
 	header('Content-Transfer-Encoding: binary');
