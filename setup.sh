b2fa825193f235b1221b33c77e9a3365dd981fc0
Ramón Román Castro <ramonromancastro@gmail.com>
2021-06-08 13:41:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 # CONSTANTES
 
 script_name="setup.sh"
-version=1.2
+version=1.2.1
 
 RES_COL=60
 MOVE_TO_COL="echo -en \\033[${RES_COL}G"
@@ -28,7 +28,7 @@ EOF
 
 # VARIABLES
 
-DESTDIR=$(dirname "$(readlink -f "$0")")
+DESTDIR=/usr/local/rrcndb
 action=
 
 # FUNCTIONS
@@ -107,7 +107,7 @@ error_msg(){
 
 check_parameters(){
 	[ -z "$DESTDIR" ] && error_msg "Installation path not defined!"
-	[ ! -d "$DESTDIR" ] && error_msg "Installation path not exists!"
+	[ -d "$DESTDIR" ] && error_msg "Installation path already exists!"
 }
 
 logo(){
@@ -135,7 +135,7 @@ do_action(){
 		;;
 		"install")
 			logo
-			install
+			do_install
 			summary
 		;;
 		*)
@@ -148,48 +148,57 @@ prerequisites(){
 	log_action "Install application prerequisites" yum -y install coreutils expect httpd perl php sed sendmail wget net-snmp-utils
 }
 
-install_parse(){
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/bin/rrcndb.sh.in > ${DESTDIR}/bin/rrcndb.sh
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/etc/rrcndb.conf.in > ${DESTDIR}/etc/rrcndb.conf
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/etc/config.in > ${DESTDIR}/etc/config
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/etc/config.exp.in > ${DESTDIR}/etc/config.exp
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/extra/rrcndb.cron.in > ${DESTDIR}/extra/rrcndb.cron
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/extra/rrcndb.httpd.in > ${DESTDIR}/extra/rrcndb.httpd
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/extra/rrcndb.logrotate.in > ${DESTDIR}/extra/rrcndb.logrotate
-	sed "s,@DESTDIR@,${DESTDIR},g" ${DESTDIR}/share/config.template.php > ${DESTDIR}/share/config.php
-}
+do_install(){
+	# Create dirs
+	install -m 755 -d ${DESTDIR}
+	install -m 755 -d ${DESTDIR}/bin
+	install -m 755 -d ${DESTDIR}/etc
+	install -m 755 -d ${DESTDIR}/extra
+	install -m 755 -d ${DESTDIR}/lib
+	install -m 755 -d ${DESTDIR}/logs
+	install -m 755 -d ${DESTDIR}/share
+	install -m 755 -d ${DESTDIR}/tmp
+	install -m 755 -d ${DESTDIR}/var
+	install -m 755 -d ${DESTDIR}/var/dump
 
-install_dirs(){
+	# Copy files
+	install -m 644 -D LICENSE ${DESTDIR}/LICENSE
+	install -m 644 -D README.md ${DESTDIR}/README.md
+	install -m 644 -D etc/router.db ${DESTDIR}/etc/router.db
+	install -m 640 -D etc/.cloginrc ${DESTDIR}/etc/.cloginrc
+	install -m 644 -D etc/htpasswd ${DESTDIR}/etc/htpasswd
+	find ./lib -type f -exec install -m 644 -D "{}" ${DESTDIR}/{} \;
+	find ./share -type f -exec install -m 644 -o root -g apache -D "{}" ${DESTDIR}/{} \;
+
+	# Copy templates
+	sed "s,@DESTDIR@,${DESTDIR},g" ./bin/rrcndb.sh.in > ${DESTDIR}/bin/rrcndb.sh
+	sed "s,@DESTDIR@,${DESTDIR},g" ./etc/rrcndb.conf.in > ${DESTDIR}/etc/rrcndb.conf
+	sed "s,@DESTDIR@,${DESTDIR},g" ./etc/config.in > ${DESTDIR}/etc/config
+	sed "s,@DESTDIR@,${DESTDIR},g" ./etc/config.exp.in > ${DESTDIR}/etc/config.exp
+	sed "s,@DESTDIR@,${DESTDIR},g" ./extra/rrcndb.cron.in > ${DESTDIR}/extra/rrcndb.cron
+	sed "s,@DESTDIR@,${DESTDIR},g" ./extra/rrcndb.httpd.in > ${DESTDIR}/extra/rrcndb.httpd
+	sed "s,@DESTDIR@,${DESTDIR},g" ./extra/rrcndb.logrotate.in > ${DESTDIR}/extra/rrcndb.logrotate
+	sed "s,@DESTDIR@,${DESTDIR},g" ./share/config.template.php > ${DESTDIR}/share/config.php
+	
+	# Configure Web GUI
+	chcon -R -t httpd_sys_content_t ${DESTDIR}/share
+	ln -fs ${DESTDIR}/extra/rrcndb.httpd /etc/httpd/conf.d/rrcndb.conf
+	
+	# Configure permissions
 	find ${DESTDIR} -type d -exec chmod 755 {} \;
 	find ${DESTDIR} -type f -exec chmod 644 {} \;
-}
-
-install_app_files(){
-	find ${DESTDIR}/bin -type f -exec chmod 755 {} \;
-	find ${DESTDIR}/etc -type f -exec chmod 600 {} \;
-	find ${DESTDIR}/includes -type f -exec chmod 640 {} \;
-	find ${DESTDIR}/lib -type f -name "*.sh" -o -name "*.exp" -exec chmod 750 {} \;
+	find ${DESTDIR}/bin -type f \( -iname "*.sh" -o -iname "*.exp" \) -exec chmod 755 {} \;
+	find ${DESTDIR}/lib -type f \( -iname "*.sh" -o -iname "*.exp" \) -exec chmod 755 {} \;
+	find ${DESTDIR}/etc -type f -exec chmod 644 {} \;
+	chmod 640 ${DESTDIR}/etc/.cloginrc
 	
-	ln -s ${DESTDIR}/extra/rrcndb.logrotate /etc/logrotate.d/rrcndb
-	ln -s ${DESTDIR}/extra/rrcndb.cron /etc/cron.d/rrcndb
-}
-
-install_webui_files(){
-	chown root:apache ${DESTDIR}/share -R
-	chcon -R -t httpd_sys_content_t ${DESTDIR}/share
-	ln -s ${DESTDIR}/extra/rrcndb.httpd /etc/httpd/conf.d/rrcndb.conf
-}
-
-install_remove_temporal(){
-	find ${DESTDIR} -type f -name "*.in" -exec rm -f {} \;
-}
-
-install(){
-	log_action "Customize installation files" install_parse
-	log_action "Create installation directories" install_dirs
-	log_action "Install base application" install_app_files
-	log_action "Install WebUI" install_webui_files
-	log_action "Remove temporary files" install_remove_temporal
+	chown -R root:apache ${DESTDIR}/share
+	
+	# Configure logrotate
+	ln -fs ${DESTDIR}/extra/rrcndb.logrotate /etc/logrotate.d/rrcndb
+	
+	# Configure cron
+	ln -fs ${DESTDIR}/extra/rrcndb.cron /etc/cron.d/rrcndb
 }
 
 version(){
